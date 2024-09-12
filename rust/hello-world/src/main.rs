72ecd51d9accc71738d4ecc35afe7368e256ea3a
babdor <babdor@gmail.com>
2024-09-12 09:12:00 -0600

    er

--- rust/hello-world/src/main.rs
+++ rust/hello-world/src/main.rs
@@ -4,7 +4,7 @@ use warp::Filter;
 #[tokio::main]
 async fn main() {
     // Match any request and return hello world!
-    let routes = warp::any().map(|| "Hello, World! -bab");
+    let routes = warp::any().map(|| "Hello, World! -babd");
 
     warp::serve(routes).run(([0, 0, 0, 0], 8080)).await;
 }
